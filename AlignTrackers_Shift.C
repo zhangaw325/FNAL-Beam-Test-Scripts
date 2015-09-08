838936a2e9dc7b823554e7fe7b908f7a24a51cf5
skaur90 <sandeep.hep@gmail.com>
2015-09-08 15:25:59 +0200

    updated

--- AlignTrackers_Shift.C
+++ AlignTrackers_Shift.C
@@ -1,6 +1,6 @@
-#include <vector.h>
+#include <vector>
 #include "doubleGausFit_withHistParameter.C"
-void main(){
+void AlignTrackers_Shift(){
   string thestring = "Position";
   string txtfilename = thestring + ".txt";
   string shiftHead = "shiftParameters_";
@@ -8,28 +8,27 @@ void main(){
   string ResidualRHead="Residual_";
   string foutname = shiftHead+thestring+"_alignTrackers.txt";
   string fout1name = residualHead+thestring+"_alignTrackers.txt";
+
+  Bool_t verbose = false;
+
   fstream fin(txtfilename.c_str(),ios::in);
   if(!fin){cout<<"file not read"<<endl; return;}
   fstream fout(foutname.c_str(),ios::out);
   fstream fout1(fout1name.c_str(),ios::out);
   double pREF2X=0.0, pREF2Y=0.0;
   double pREF3X=0.0, pREF3Y=0.0;
-  double pUVA3X=0.0, pUVA3Y=0.0;
   double pREF1X=0.0, pREF1Y=0.0;
-  //double pZZ1=0.0, pZZ2=0.0;
-  double pEta5=0.0;
+
   vector<double> vpREF2X; vector<double> vpREF2Y;
   vector<double> vpREF3X; vector<double> vpREF3Y;
-  vector<double> vpUVA3X; vector<double> vpUVA3Y;
   vector<double> vpREF1X; vector<double> vpREF1Y;
-  //vector<double> vpZZ1; vector<double> vpZZ2;
- // vector<double> vpEta5;
+
   Int_t nbLines=0;
-  while(fin>>pREF2X>>pREF2Y>>pREF3X>>pREF3Y>>pUVA3X>>pUVA3Y>>pREF1X>>pREF1Y/*>>pEta5*/){
+  while(fin>>pREF2X>>pREF2Y>>pREF3X>>pREF3Y>>pREF1X>>pREF1Y/*>>pEta5*/){
     vpREF2X.push_back(pREF2X); vpREF2Y.push_back(pREF2Y); vpREF3X.push_back(pREF3X); vpREF3Y.push_back(pREF3Y);
-    vpUVA3X.push_back(pUVA3X); vpUVA3Y.push_back(pUVA3Y); vpREF1X.push_back(pREF1X); vpREF1Y.push_back(pREF1Y);
-    //vpZZ1.push_back(pZZ1); vpZZ2.push_back(pZZ2);
-   // vpEta5.push_back(pEta5);
+    vpREF1X.push_back(pREF1X); vpREF1Y.push_back(pREF1Y);
+    if (verbose)
+	cout<<pREF2X<<"\t"<<pREF2Y<<"\t"<<pREF3X<<"\t"<<pREF3Y<<"\t"<<pREF1X<<"\t"<<pREF1Y<<endl;
     nbLines++;
   }
   fin.close();
@@ -38,10 +37,10 @@ void main(){
       [-30,-20]: -22.98, 1.089, -28.29, -3.209, -37.22, 14.88, -46.62, -2.411, 29.84
       [-20,-15]: -17.07, 0.4154, -22.27, -3.92, -31.12, 14.55, -40.55, -2.637, 29.48
   */
-  double shiREF2X=12.25640557, shiREF2Y=2.03531933;
-  double shiREF3X=7.467908772, shiREF3Y=-2.35761;
-  double shiUVA3X=-0.73056, shiUVA3Y=15.74758;
-  double shiREF1X=-9.94956, shiREF1Y=-0.70242;
+  double shiREF2X=62.36	, shiREF2Y=55.83;
+  double shiREF3X=62.72	, shiREF3Y=56.06;
+  double shiUVA3X=62.72 , shiUVA3Y=56.06;
+  double shiREF1X=61.91 , shiREF1Y=55.68;
   //double shiZZ1=0,   shiZZ2=0;
   //double shiEta5=30.1;
 
@@ -61,38 +60,27 @@ void main(){
     iterNb++;
     char name2X[15];sprintf(name2X,"posREF2X_%i",iterNb); char name2Y[15];sprintf(name2Y,"posREF2Y_%i",iterNb);
     char name3X[15];sprintf(name3X,"posREF3X_%i",iterNb); char name3Y[15];sprintf(name3Y,"posREF3Y_%i",iterNb);
-    char nameu3X[15];sprintf(nameu3X,"posUVA3X_%i",iterNb);char nameu3Y[15];sprintf(nameu3Y,"posUVA3Y_%i",iterNb);
     char name1X[15];sprintf(name1X,"posREF1X_%i",iterNb); char name1Y[15];sprintf(name1Y,"posREF1Y_%i",iterNb);
-    //char nameZZ1[15];sprintf(nameZZ1,"pos10cmZZ1_%i",iterNb); char nameZZ2[15];sprintf(nameZZ2,"pos10cmZZ2_%i",iterNb);
-    //char nameEta5[15];sprintf(nameEta5,"posEta5_%i",iterNb); 
-    TH1F* hpREF2X = new TH1F(name2X,"",500,-50,50); hpREF2X->SetXTitle("mm"); hpREF2X->SetYTitle("Frequency");hpREF2X->SetLabelSize(0.045,"XY");hpREF2X->SetTitleSize(0.045,"XY");
-    TH1F* hpREF2Y = new TH1F(name2Y,"",500,-50,50); hpREF2Y->SetXTitle("mm"); hpREF2Y->SetYTitle("Frequency");hpREF2Y->SetLabelSize(0.045,"XY");hpREF2Y->SetTitleSize(0.045,"XY");  
-    TH1F* hpREF3X = new TH1F(name3X,"",500,-50,50); hpREF3X->SetXTitle("mm"); hpREF3X->SetYTitle("Frequency");hpREF3X->SetLabelSize(0.045,"XY");hpREF3X->SetTitleSize(0.045,"XY");
-    TH1F* hpREF3Y = new TH1F(name3Y,"",500,-50,50); hpREF3Y->SetXTitle("mm"); hpREF3Y->SetYTitle("Frequency");hpREF3Y->SetLabelSize(0.045,"XY");hpREF3Y->SetTitleSize(0.045,"XY");  
-    TH1F* hpUVA3X = new TH1F(nameu3X,"",500,-50,50); hpUVA3X->SetXTitle("mm"); hpUVA3X->SetYTitle("Frequency");hpUVA3X->SetLabelSize(0.045,"XY");hpUVA3X->SetTitleSize(0.045,"XY");
-    TH1F* hpUVA3Y = new TH1F(nameu3Y,"",500,-50,50); hpUVA3Y->SetXTitle("mm"); hpUVA3Y->SetYTitle("Frequency");hpUVA3Y->SetLabelSize(0.045,"XY");hpUVA3Y->SetTitleSize(0.045,"XY");  
-    TH1F* hpREF1X = new TH1F(name1X,"",500,-50,50); hpREF1X->SetXTitle("mm"); hpREF1X->SetYTitle("Frequency");hpREF1X->SetLabelSize(0.045,"XY");hpREF1X->SetTitleSize(0.045,"XY");
-    TH1F* hpREF1Y = new TH1F(name1Y,"",500,-50,50); hpREF1Y->SetXTitle("mm"); hpREF1Y->SetYTitle("Frequency");hpREF1Y->SetLabelSize(0.045,"XY");hpREF1Y->SetTitleSize(0.045,"XY");  
-    //TH1F* hpZZ1 = new TH1F(nameZZ1,"",600,-150,150); hpZZ1->SetXTitle("mm"); hpZZ1->SetYTitle("Frequency"); hpZZ1->SetLabelSize(0.045,"XY");hpZZ1->SetTitleSize(0.045,"XY");
-    //TH1F* hpZZ2 = new TH1F(nameZZ2,"",600,-150,150); hpZZ2->SetXTitle("mm"); hpZZ2->SetYTitle("Frequency"); hpZZ2->SetLabelSize(0.045,"XY");hpZZ2->SetTitleSize(0.045,"XY");
-    //TH1F* hpEta5 = new TH1F(nameEta5,"",600,-150,150); hpEta5->SetXTitle("mm"); hpEta5->SetYTitle("Frequency"); hpEta5->SetLabelSize(0.045,"XY");hpEta5->SetTitleSize(0.045,"XY");
+    if (verbose)
+	cout<<name1X<<"\t"<<name2X<<"\t"<<name3X<<endl;
+    TH1F* hpREF2X = new TH1F(name2X,"",500,-10,110); hpREF2X->SetXTitle("mm"); hpREF2X->SetYTitle("Frequency");hpREF2X->SetLabelSize(0.045,"XY");hpREF2X->SetTitleSize(0.045,"XY");
+    TH1F* hpREF2Y = new TH1F(name2Y,"",500,-10,110); hpREF2Y->SetXTitle("mm"); hpREF2Y->SetYTitle("Frequency");hpREF2Y->SetLabelSize(0.045,"XY");hpREF2Y->SetTitleSize(0.045,"XY");  
+    TH1F* hpREF3X = new TH1F(name3X,"",500,-10,110); hpREF3X->SetXTitle("mm"); hpREF3X->SetYTitle("Frequency");hpREF3X->SetLabelSize(0.045,"XY");hpREF3X->SetTitleSize(0.045,"XY");
+    TH1F* hpREF3Y = new TH1F(name3Y,"",500,-10,110); hpREF3Y->SetXTitle("mm"); hpREF3Y->SetYTitle("Frequency");hpREF3Y->SetLabelSize(0.045,"XY");hpREF3Y->SetTitleSize(0.045,"XY");  
+    TH1F* hpREF1X = new TH1F(name1X,"",500,-10,110); hpREF1X->SetXTitle("mm"); hpREF1X->SetYTitle("Frequency");hpREF1X->SetLabelSize(0.045,"XY");hpREF1X->SetTitleSize(0.045,"XY");
+    TH1F* hpREF1Y = new TH1F(name1Y,"",500,-10,110); hpREF1Y->SetXTitle("mm"); hpREF1Y->SetYTitle("Frequency");hpREF1Y->SetLabelSize(0.045,"XY");hpREF1Y->SetTitleSize(0.045,"XY");  
     char nameRes2X[20];sprintf(nameRes2X,"residualREF2X_%i",iterNb);char nameRes2Y[20];sprintf(nameRes2Y,"residualREF2Y_%i",iterNb);
     char nameRes3X[20];sprintf(nameRes3X,"residualREF3X_%i",iterNb);char nameRes3Y[20];sprintf(nameRes3Y,"residualREF3Y_%i",iterNb);
-    char nameResu3X[20];sprintf(nameResu3X,"residualUVA3X_%i",iterNb);char nameResu3Y[20];sprintf(nameResu3Y,"residualUVA3Y_%i",iterNb);
     char nameRes1X[20];sprintf(nameRes1X,"residualREF1X_%i",iterNb);char nameRes1Y[20];sprintf(nameRes1Y,"residualREF1Y_%i",iterNb);
-    //char nameResZZ1[20];sprintf(nameResZZ1,"residualZZ1_%i",iterNb);char nameResZZ2[20];sprintf(nameResZZ2,"residualZZ2_%i",iterNb);
-    //char nameResEta5[20];sprintf(nameResEta5,"residualEta5_%i",iterNb);
+    if (verbose)
+	cout<<nameRes1X<<"\t"<<nameRes2X<<"\t"<<nameRes3X<<endl;
+
     TH1F* residualREF2X = new TH1F(nameRes2X,"",800,-8,8); residualREF2X->SetXTitle("Residual [mm]"); residualREF2X->SetYTitle("Frequency");residualREF2X->SetLabelSize(0.045,"XY");residualREF2X->SetTitleSize(0.045,"XY");
     TH1F* residualREF2Y = new TH1F(nameRes2Y,"",800,-8,8); residualREF2Y->SetXTitle("Residual [mm]"); residualREF2Y->SetYTitle("Frequency");residualREF2Y->SetLabelSize(0.045,"XY");residualREF2Y->SetTitleSize(0.045,"XY");
     TH1F* residualREF3X = new TH1F(nameRes3X,"",800,-8,8); residualREF3X->SetXTitle("Residual [mm]"); residualREF3X->SetYTitle("Frequency");residualREF3X->SetLabelSize(0.045,"XY");residualREF3X->SetTitleSize(0.045,"XY");
     TH1F* residualREF3Y = new TH1F(nameRes3Y,"",800,-8,8); residualREF3Y->SetXTitle("Residual [mm]"); residualREF3Y->SetYTitle("Frequency");residualREF3Y->SetLabelSize(0.045,"XY");residualREF3Y->SetTitleSize(0.045,"XY");
-    TH1F* residualUVA3X = new TH1F(nameResu3X,"",800,-8,8); residualUVA3X->SetXTitle("Residual [mm]"); residualUVA3X->SetYTitle("Frequency");residualUVA3X->SetLabelSize(0.045,"XY");residualUVA3X->SetTitleSize(0.045,"XY");
-    TH1F* residualUVA3Y = new TH1F(nameResu3Y,"",800,-8,8); residualUVA3Y->SetXTitle("Residual [mm]"); residualUVA3Y->SetYTitle("Frequency");residualUVA3Y->SetLabelSize(0.045,"XY");residualUVA3Y->SetTitleSize(0.045,"XY");
     TH1F* residualREF1X = new TH1F(nameRes1X,"",800,-8,8); residualREF1X->SetXTitle("mm"); residualREF1X->SetYTitle("Frequency");residualREF1X->SetLabelSize(0.045,"XY");residualREF1X->SetTitleSize(0.045,"XY");
     TH1F* residualREF1Y = new TH1F(nameRes1Y,"",800,-8,8); residualREF1Y->SetXTitle("mm"); residualREF1Y->SetYTitle("Frequency");residualREF1Y->SetLabelSize(0.045,"XY");residualREF1Y->SetTitleSize(0.045,"XY");
-    //TH1F* residualZZ1 = new TH1F(nameResZZ1,"",320,-16,16); residualZZ1->SetXTitle("Residual [mm]"); residualZZ1->SetYTitle("Frequency");residualZZ1->SetLabelSize(0.045,"XY");residualZZ1->SetTitleSize(0.045,"XY");
-    //TH1F* residualZZ2 = new TH1F(nameResZZ2,"",320,-16,16); residualZZ2->SetXTitle("Residual [mm]"); residualZZ2->SetYTitle("Frequency");residualZZ2->SetLabelSize(0.045,"XY");residualZZ2->SetTitleSize(0.045,"XY");
-    //TH1F* residualEta5 = new TH1F(nameResEta5,"",320,-16,16); residualEta5->SetXTitle("Residual [mm]"); residualEta5->SetYTitle("Frequency");residualEta5->SetLabelSize(0.045,"XY");residualEta5->SetTitleSize(0.045,"XY");
     /*
     char nameDxREF3X[20]; sprintf(nameDxREF3X,"REF3X_REF2X_%i",iterNb); char nameDyREF3Y[20]; sprintf(nameDyREF3Y,"REF3Y_REF2Y_%i",iterNb); 
     char nameDxUVA3X[20]; sprintf(nameDxUVA3X,"UVA3X_REF2X_%i",iterNb); char nameDyUVA3Y[20]; sprintf(nameDyUVA3Y,"UVA3Y_REF2Y_%i",iterNb); 
@@ -118,56 +106,42 @@ void main(){
     //deltayZZ->SetLabelSize(0.045,"XY");deltayZZ->SetTitleSize(0.045,"XY");
     //cout<<"shift: "<<shiREF2X<<"\t"<<shiREF2Y<<"\t"<<shiREF3X<<"\t"<<shiREF3Y<<"\t"<<shiUVA3X<<"\t"<<shiUVA3Y<<"\t"<<shiREF1X<<"\t"<<shiREF1Y<<"\t"<<shiEta5<<endl;
     fout<<shiREF2X<<"\t"<<shiREF2Y<<"\t"<<shiREF3X<<"\t"<<shiREF3Y<<"\t"<<shiUVA3X<<"\t"<<shiUVA3Y<<"\t"<<shiREF1X<<"\t"<<shiREF1Y<<endl;
+    if (verbose)
+    cout<<shiREF2X<<"\t"<<shiREF2Y<<"\t"<<shiREF3X<<"\t"<<shiREF3Y<<"\t"<<shiUVA3X<<"\t"<<shiUVA3Y<<"\t"<<shiREF1X<<"\t"<<shiREF1Y<<endl;
     int nnnn=0;
     for(Int_t i=0;i<vpREF2X.size();i++){
       vpREF2X[i] = vpREF2X[i] - shiREF2X; vpREF2Y[i] = vpREF2Y[i] - shiREF2Y;      
       vpREF3X[i] = vpREF3X[i] - shiREF3X; vpREF3Y[i] = vpREF3Y[i] - shiREF3Y;
-      vpUVA3X[i] = vpUVA3X[i] - shiUVA3X; vpUVA3Y[i] = vpUVA3Y[i] - shiUVA3Y;
       vpREF1X[i] = vpREF1X[i] - shiREF1X; vpREF1Y[i] = vpREF1Y[i] - shiREF1Y;
-     // vpZZ1[i] = vpZZ1[i] - shiZZ1; vpZZ2[i] = vpZZ2[i] - shiZZ2;
-      //vpEta5[i] = vpEta5[i] - shiEta5; 
       hpREF2X->Fill(vpREF2X[i]); hpREF2Y->Fill(vpREF2Y[i]);
       hpREF3X->Fill(vpREF3X[i]); hpREF3Y->Fill(vpREF3Y[i]);
-      hpUVA3X->Fill(vpUVA3X[i]); hpUVA3Y->Fill(vpUVA3Y[i]);
       hpREF1X->Fill(vpREF1X[i]); hpREF1Y->Fill(vpREF1Y[i]);
-      //hpZZ1->Fill(vpZZ1[i]); hpZZ2->Fill(vpZZ2[i]);
-     // hpEta5->Fill(vpEta5[i]); //cout<<vpEta5[i]<<endl;
       
-      //dxREF3X->Fill(vpREF3X[i]-vpREF2X[i]); dyREF3Y->Fill(vpREF3Y[i]-vpREF2Y[i]);
-      //dxUVA3X->Fill(vpUVA3X[i]-vpREF2X[i]); dyUVA3Y->Fill(vpUVA3Y[i]-vpREF2Y[i]);
-      //dxREF1X->Fill(vpREF1X[i]-vpREF2X[i]); dyREF1Y->Fill(vpREF1Y[i]-vpREF2Y[i]);
-      //dxUVA3REF3->Fill(vpUVA3X[i]-vpREF3X[i]); dyUVA3REF3->Fill(vpUVA3Y[i]-vpREF3Y[i]);
-      //dxREF1UVA3->Fill(vpREF1X[i]-vpUVA3X[i]); dyREF1UVA3->Fill(vpREF1Y[i]-vpUVA3Y[i]);
+      if (verbose)
+	  cout<<i<<"\t"<<vpREF1X[i]<<"\t"<<vpREF1Y[i]<<"\t"<<vpREF2X[i]<<"\t"<<vpREF2Y[i]<<"\t"<<vpREF3X[i]<<"\t"<<vpREF3Y[i]<<endl;
 
       TGraph* g1 = new TGraph();
-      g1->SetPoint(0,0,     vpREF2X[i]);
-      g1->SetPoint(1,1143.5,vpREF3X[i]);
-      g1->SetPoint(2,2686.5,vpUVA3X[i]);
-      g1->SetPoint(3,3169.5,vpREF1X[i]);
+      g1->SetPoint(0,100,     vpREF2X[i]);
+      g1->SetPoint(1,380.,vpREF3X[i]);
+      g1->SetPoint(3,830.,vpREF1X[i]);
       TF1* f1 = new TF1("line1","[0]+[1]*x",0,3200);
       g1->Fit("line1","Q");
       double intercept1 = f1->GetParameter(0);
       double slope1     = f1->GetParameter(1);
-      double MeasuredREF2X = intercept1 + slope1*0.0;
-      double MeasuredREF3X = intercept1 + slope1*1143.5;
-      double MeasuredUVA3X = intercept1 + slope1*2686.5;
-      double MeasuredREF1X = intercept1 + slope1*3169.5;
+      double MeasuredREF2X = intercept1 + slope1*100.;
+      double MeasuredREF3X = intercept1 + slope1*380.;
+      double MeasuredREF1X = intercept1 + slope1*830.;
       residualREF2X->Fill(MeasuredREF2X-vpREF2X[i]);
       residualREF3X->Fill(MeasuredREF3X-vpREF3X[i]);
-      residualUVA3X->Fill(MeasuredUVA3X-vpUVA3X[i]);
       residualREF1X->Fill(MeasuredREF1X-vpREF1X[i]);
       delete f1; delete g1;
 
       TGraph* g2 = new TGraph();
-      g2->SetPoint(0,0,     vpREF2Y[i]);
-      g2->SetPoint(1,1143.5,vpREF3Y[i]);
+      g2->SetPoint(0,100.,     vpREF2Y[i]);
+      g2->SetPoint(1,380.,vpREF3Y[i]);
       // exclusive
-       g2->SetPoint(2,2686.5,vpUVA3Y[i]);
-       g2->SetPoint(3,3169.5,vpREF1Y[i]);
+       g2->SetPoint(3,830.,vpREF1Y[i]);
       // inclusive
-      //g2->SetPoint(2,2305.5,vpZZ2[i]); //inclusive 1
-      //g2->SetPoint(2,2327.5,vpZZ1[i]);  //inclusive 2
-      //g2->SetPoint(3,2686.5,vpUVA3Y[i]);
 
 
       //g2->SetPoint(4,3169.5,vpREF1Y[i]);
@@ -176,16 +150,14 @@ void main(){
       g2->Fit("line2","Q");
       double intercept2 = f2->GetParameter(0);
       double slope2     = f2->GetParameter(1);
-      double MeasuredREF2Y = intercept2 + slope2*0.0;
-      double MeasuredREF3Y = intercept2 + slope2*1143.5;
-      double MeasuredUVA3Y = intercept2 + slope2*2686.5;
-      double MeasuredREF1Y = intercept2 + slope2*3169.5;
+      double MeasuredREF2Y = intercept2 + slope2*100.;
+      double MeasuredREF3Y = intercept2 + slope2*380.;
+      double MeasuredREF1Y = intercept2 + slope2*830.;
       //double MeasuredZZ1  = intercept2 + slope2*2327.5;
       //double MeasuredZZ2  = intercept2 + slope2*2305.5;
       //double MeasuredEta5 = intercept2 + slope2*2011.5;
       residualREF2Y->Fill(MeasuredREF2Y-vpREF2Y[i]);
       residualREF3Y->Fill(MeasuredREF3Y-vpREF3Y[i]);
-      residualUVA3Y->Fill(MeasuredUVA3Y-vpUVA3Y[i]);
       residualREF1Y->Fill(MeasuredREF1Y-vpREF1Y[i]);
       //residualZZ1->Fill(MeasuredZZ1-vpZZ1[i]);
       //residualZZ2->Fill(MeasuredZZ2-vpZZ2[i]);
@@ -197,25 +169,22 @@ void main(){
       //if(nnnn>1000) break;
     } //for loop
     //cout<<"after for loop"<<endl;
-    gStyle->SetOptFit(1111);
-    
-    I2GFvalues myValues;
-    myValues = I2GFmainLoop(residualREF2X,1,10,1);
-    meanREF2X = myValues.mean; //sigmaEta5=myValues.sigma;
-    myValues = I2GFmainLoop(residualREF2Y,1,10,1);
-    meanREF2Y = myValues.mean; //
-    myValues = I2GFmainLoop(residualREF3X,1,10,1);
-    meanREF3X = myValues.mean; //
-    myValues = I2GFmainLoop(residualREF3Y,1,10,1);
-    meanREF3Y = myValues.mean;
-    myValues = I2GFmainLoop(residualUVA3X,1,10,1);
-    meanUVA3X = myValues.mean; //
-    myValues = I2GFmainLoop(residualUVA3Y,1,10,1);
-    meanUVA3Y = myValues.mean;
-    myValues = I2GFmainLoop(residualREF1X,1,10,1);
-    meanREF1X = myValues.mean; //
-    myValues = I2GFmainLoop(residualREF1Y,1,10,1);
-    meanREF1Y = myValues.mean;
+//
+//    gStyle->SetOptFit(1111);
+//    
+//    I2GFvalues myValues;
+//    myValues = I2GFmainLoop(residualREF2X,1,10,1);
+//    meanREF2X = myValues.mean; //sigmaEta5=myValues.sigma;
+//    myValues = I2GFmainLoop(residualREF2Y,1,10,1);
+//    meanREF2Y = myValues.mean; //
+//    myValues = I2GFmainLoop(residualREF3X,1,10,1);
+//    meanREF3X = myValues.mean; //
+//    myValues = I2GFmainLoop(residualREF3Y,1,10,1);
+//    meanREF3Y = myValues.mean;
+//    myValues = I2GFmainLoop(residualREF1X,1,10,1);
+//    meanREF1X = myValues.mean; //
+//    myValues = I2GFmainLoop(residualREF1Y,1,10,1);
+//    meanREF1Y = myValues.mean;
     /*
     double maximum = residualREF2X->GetMean();
     double rms = residualREF2X->GetRMS(1);
@@ -283,9 +252,9 @@ void main(){
     lRange = maximum - rms*1.4; hRange = maximum + rms*1.4;//1.4
     TF1* funDeltaYZZ = new TF1("funDeltaYZZ","gaus",lRange,hRange);funDeltaYZZ->SetLineColor(kBlue); deltayZZ->Fit("funDeltaYZZ","RQ");
     */
-    cout<<"residual mean: "<<meanREF2X<<"\t"<<meanREF2Y<<"\t"<<meanREF3X<<"\t"<<meanREF3Y<<"\t"<<meanUVA3X<<"\t"<<meanUVA3Y<<"\t"<<meanREF1X<<"\t"<<meanREF1Y<<endl;
+    cout<<"residual mean: "<<meanREF2X<<"\t"<<meanREF2Y<<"\t"<<meanREF3X<<"\t"<<meanREF3Y<<"\t"<<"\t"<<meanREF1X<<"\t"<<meanREF1Y<<endl;
         //<<meanZZ1<<"\t"<<meanZZ2<<endl;
-    fout1<<"residual mean: "<<meanREF2X<<"\t"<<meanREF2Y<<"\t"<<meanREF3X<<"\t"<<meanREF3Y<<"\t"<<meanUVA3X<<"\t"<<meanUVA3Y<<"\t"<<meanREF1X<<"\t"<<meanREF1Y<<endl;
+    fout1<<"residual mean: "<<meanREF2X<<"\t"<<meanREF2Y<<"\t"<<meanREF3X<<"\t"<<meanREF3Y<<"\t"<<"\t"<<meanREF1X<<"\t"<<meanREF1Y<<endl;
         //<<meanZZ1<<"\t"<<meanZZ2<<endl;
     /*    
     maximum = dxREF3X->GetMean(); rms=dxREF3X->GetRMS(1); lRange=maximum-rms*1.1; hRange=maximum+1.10*rms;
@@ -319,7 +288,6 @@ void main(){
     double factor = -0.1;
     shiREF2X = meanREF2X*factor; shiREF2Y = meanREF2Y*factor; 
     shiREF3X = meanREF3X*factor; shiREF3Y = meanREF3Y*factor; 
-    shiUVA3X = meanUVA3X*factor; shiUVA3Y = meanUVA3Y*factor; 
     shiREF1X = meanREF1X*factor; shiREF1Y = meanREF1Y*factor; 
     //shiZZ1 = meanZZ1*factor; shiZZ2 = meanZZ2*factor; 
     //shiEta5 = meanEta5*factor;
@@ -327,7 +295,6 @@ void main(){
     //break;
     if((meanREF2X>=-0.0005 && meanREF2X<=0.0005) && (meanREF2Y>=-0.0005 && meanREF2Y<=0.0005))
       if((meanREF3X>=-0.0005 && meanREF3X<=0.0005) && (meanREF3Y>=-0.0005 && meanREF3Y<=0.0005))
-        if((meanUVA3X>=-0.0005 && meanUVA3X<=0.0005) && (meanUVA3Y>=-0.0005 && meanUVA3Y<=0.0005))
         	if((meanREF1X>=-0.0005 && meanREF1X<=0.0005) && (meanREF1Y>=-0.0005 && meanREF1Y<=0.0005))
           {
             cout<<"find it...iterating "<<iterNb<<" times."<<endl;
