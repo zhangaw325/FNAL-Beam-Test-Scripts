0913fab2ad817327e0288b99f6a79cb6685b2846
ram1123 <ramkrishna.sharma71@gmail.com>
2015-09-13 17:57:22 +0530

    bug fixed in doubleGausFit_withHistParameter.C, But need to cross-checkgit add AlignTrackers_Shift.C Hit_Position_Info.txt doubleGausFit_withHistParameter.C

--- AlignTrackers_Shift.C
+++ AlignTrackers_Shift.C
@@ -28,6 +28,8 @@
   std::vector<double> vPos_g3xcl; std::vector<double> vPos_g3ycl;
   std::vector<double> vPos_g1xcl; std::vector<double> vPos_g1ycl;
 
+  bool verbose = 0;
+
   Int_t nbLines=0;
   while(fin>>Pos_g2xcl>>Pos_g2ycl>>Pos_g3xcl>>Pos_g3ycl>>Pos_g1xcl>>Pos_g1ycl){//pUVA3X>>pUVA3Y>>pREF1X>>pREF1Y/*>>pEta5*/){
     vPos_g2xcl.push_back(Pos_g2xcl); vPos_g2ycl.push_back(Pos_g2ycl); 
@@ -48,7 +50,11 @@
 
 
   Int_t iterNb=0;
+  if (verbose)
+      cout<<"Some problem 1"<<endl;
   while(1){
+  if (verbose)
+      cout<<"Some problem 2"<<endl;
     char rootfile[50]; sprintf(rootfile,"_iter_%i.root",iterNb);
     string outputrootname=ResidualRHead+thestring+rootfile;
     TFile* f = new TFile(outputrootname.c_str(),"recreate");  
@@ -56,6 +62,8 @@
     char name2X[15];sprintf(name2X,"Pos_g2xcl_%i",iterNb); char name2Y[15];sprintf(name2Y,"Pos_g2ycl_%i",iterNb);
     char name3X[15];sprintf(name3X,"Pos_g3xcl_%i",iterNb); char name3Y[15];sprintf(name3Y,"Pos_g3ycl_%i",iterNb);
     char name1X[15];sprintf(name1X,"Pos_g1xcl_%i",iterNb); char name1Y[15];sprintf(name1Y,"Pos_g1xcl_%i",iterNb);
+  if (verbose)
+      cout<<"Some problem 3"<<endl;
     TH1F* h_Pos_g2xcl = new TH1F(name2X,"",100,-10,110); h_Pos_g2xcl->SetXTitle("Cluster position [mm]"); h_Pos_g2xcl->SetYTitle("Frequency");h_Pos_g2xcl->SetLabelSize(0.045,"XY");h_Pos_g2xcl->SetTitleSize(0.045,"XY");
     TH1F* h_Pos_g2ycl = new TH1F(name2Y,"",100,-10,110); h_Pos_g2ycl->SetXTitle(" Cluster position [mm]"); h_Pos_g2ycl->SetYTitle("Frequency");h_Pos_g2ycl->SetLabelSize(0.045,"XY");h_Pos_g2ycl->SetTitleSize(0.045,"XY");  
     TH1F* h_Pos_g1xcl = new TH1F(name3X,"",100,-10,110); h_Pos_g1xcl->SetXTitle("Cluster position [mm]"); h_Pos_g1xcl->SetYTitle("Frequency");h_Pos_g1xcl->SetLabelSize(0.045,"XY");h_Pos_g1xcl->SetTitleSize(0.045,"XY");
@@ -65,6 +73,8 @@
     TH1F* h_Pos_g3xcl = new TH1F(name1X,"",100,-10,110); h_Pos_g3xcl->SetXTitle("Cluster position [mm]"); h_Pos_g3xcl->SetYTitle("Frequency");h_Pos_g3xcl->SetLabelSize(0.045,"XY");h_Pos_g3xcl->SetTitleSize(0.045,"XY");
     TH1F* h_Pos_g3ycl = new TH1F(name1Y,"",100,-10,110); h_Pos_g3ycl->SetXTitle(" Cluster position [mm]"); h_Pos_g3ycl->SetYTitle("Frequency");h_Pos_g3ycl->SetLabelSize(0.045,"XY");h_Pos_g3ycl->SetTitleSize(0.045,"XY");  
 
+  if (verbose)
+      cout<<"Some problem 4"<<endl;
     char nameRes2X[20];sprintf(nameRes2X,"residual_g2xcl_%i",iterNb);char nameRes2Y[20];sprintf(nameRes2Y,"residual_g2ycl_%i",iterNb);
     char nameRes3X[20];sprintf(nameRes3X,"residual_g3xcl_%i",iterNb);char nameRes3Y[20];sprintf(nameRes3Y,"residual_g3ycl_%i",iterNb);
 
@@ -81,13 +91,22 @@
 
     fout<<shi_g2xcl<<"\t"<<shi_g2ycl<<"\t"<<shi_g3xcl<<"\t"<<shi_g3ycl<<"\t"<<shi_g1xcl<<"\t"<<shi_g1ycl<<endl;
 
+  if (verbose)
+      cout<<"Some problem 5"<<endl;
     int nnnn=0;
-    TGraph* g1 = new TGraph();
-    TF1* f1 = new TF1("line1","[0]+[1]*x",0,3200);
-    TGraph* g2 = new TGraph();
-    TF1* f2 = new TF1("line2","[0]+[1]*x",0,3200);
+    //TGraph* g1 = new TGraph();
+    //TF1* f1 = new TF1("line1","[0]+[1]*x",0,900);
+    //TGraph* g2 = new TGraph();
+    //TF1* f2 = new TF1("line2","[0]+[1]*x",0,900);
 
+  if (verbose)
+      cout<<"Some problem 6"<<endl;
     for(unsigned int i=0;i<vPos_g2xcl.size();i++){
+//    for(unsigned int i=0;i<500;i++){
+    TGraph* g1 = new TGraph();
+    TF1* f1 = new TF1("line1","[0]+[1]*x",0,900);
+    TGraph* g2 = new TGraph();
+    TF1* f2 = new TF1("line2","[0]+[1]*x",0,900);
       vPos_g2xcl[i] = vPos_g2xcl[i] - shi_g2xcl; vPos_g2ycl[i] = vPos_g2ycl[i] - shi_g2ycl;      
       vPos_g3xcl[i] = vPos_g3xcl[i] - shi_g3xcl; vPos_g3ycl[i] = vPos_g3ycl[i] - shi_g3ycl;
       vPos_g1xcl[i] = vPos_g1xcl[i] - shi_g1xcl; vPos_g1ycl[i] = vPos_g1ycl[i] - shi_g1ycl;
@@ -97,22 +116,37 @@
       h_Pos_g1xcl->Fill(vPos_g1xcl[i]); h_Pos_g1ycl->Fill(vPos_g1ycl[i]);
 
 
+  if (verbose)
+      cout<<"Some problem 7"<<endl;
       //      TGraph* g1 = new TGraph();
-      g1->SetPoint(0,0,     vPos_g1xcl[i]);
-      g1->SetPoint(1,380,vPos_g2xcl[i]);
-      g1->SetPoint(2,830,vPos_g3xcl[i]);
+  if (verbose)
+      cout<<"RK0"<<endl;
+      g1->SetPoint(0,0., vPos_g1xcl[i]);
+  if (verbose)
+      cout<<"RK1"<<endl;
+      g1->SetPoint(1,380.,vPos_g2xcl[i]);
+  if (verbose)
+      cout<<"RK2"<<endl;
+      g1->SetPoint(2,830.,vPos_g3xcl[i]);
+  if (verbose)
+      cout<<"RK3"<<endl;
       //      TF1* f1 = new TF1("line1","[0]+[1]*x",0,3200);
+      if (verbose)
+	  cout<<"vPos_g1xcl["<<i<<"] = "<<vPos_g1xcl[i]<<"\tvPos_g2xcl["<<i<<"] = "<<vPos_g2xcl[i]<<"\tvPos_g3xcl["<<i<<"] = "<<vPos_g3xcl[i]<<endl;
       g1->Fit("line1","Q");
       double intercept1 = f1->GetParameter(0);
       double slope1     = f1->GetParameter(1);
       double Measured_g1xcl = intercept1 + slope1*0.0;
-      double Measured_g2xcl = intercept1 + slope1*380;
-      double Measured_g3xcl = intercept1 + slope1*830;
+      double Measured_g2xcl = intercept1 + slope1*380.;
+      double Measured_g3xcl = intercept1 + slope1*830.;
+      if (verbose)
+	  cout<<"intercept1 = "<<intercept1<<"\tslope1 = "<<slope1 <<endl;
       h_residual_g2xcl->Fill(Measured_g2xcl-vPos_g2xcl[i]);
       h_residual_g3xcl->Fill(Measured_g3xcl-vPos_g3xcl[i]);
       h_residual_g1xcl->Fill(Measured_g1xcl-vPos_g1xcl[i]);
       delete f1; delete g1;
-
+  if (verbose)
+      cout<<"Some problem 8"<<endl;
       //      TGraph* g2 = new TGraph();
       g2->SetPoint(0,0,     vPos_g1ycl[i]);
       g2->SetPoint(1,380,vPos_g2ycl[i]);
@@ -129,31 +163,63 @@
       double Measured_g2ycl = intercept2 + slope2*380;
       double Measured_g3ycl = intercept2 + slope2*830;
 
+  if (verbose)
+      cout<<"Some problem 9"<<endl;
       h_residual_g2ycl->Fill(Measured_g2ycl-vPos_g2ycl[i]);
       h_residual_g3ycl->Fill(Measured_g3ycl-vPos_g3ycl[i]);
 
       h_residual_g1ycl->Fill(Measured_g1ycl-vPos_g1ycl[i]);
+  if (verbose)
+      cout<<"Some problem 10"<<endl;
       delete f2; delete g2;
+  if (verbose)
+      cout<<"Some problem 11"<<endl;
       nnnn++;
+  if (verbose)
+      cout<<"Some problem 12"<<endl;
 
     }//for loop g2xcl size
 
     gStyle->SetOptFit(1111);
     
+  if (verbose)
+      cout<<"Some problem 13"<<endl;
     I2GFvalues myValues;
+  if (verbose)
+      cout<<"Some problem 13a"<<endl;
+  
+  h_residual_g2xcl->Draw();
+      //cin.get();
+
     myValues = I2GFmainLoop(h_residual_g2xcl,1,10,1);
+  if (verbose)
+      cout<<"Some problem 13b"<<endl;
     mean_g2xcl = myValues.mean; //sigmaEta5=myValues.sigma;
+  if (verbose)
+      cout<<"Some problem 13c"<<endl;
     myValues = I2GFmainLoop(h_residual_g2ycl,1,10,1);
+  if (verbose)
+      cout<<"Some problem 14"<<endl;
     mean_g2ycl = myValues.mean; //
     myValues = I2GFmainLoop(h_residual_g3xcl,1,10,1);
+  if (verbose)
+      cout<<"Some problem 15"<<endl;
     mean_g3xcl = myValues.mean; //
     myValues = I2GFmainLoop(h_residual_g3ycl,1,10,1);
+  if (verbose)
+      cout<<"Some problem 16"<<endl;
     mean_g3ycl = myValues.mean;
 
     myValues = I2GFmainLoop(h_residual_g1xcl,1,10,1);
+  if (verbose)
+      cout<<"Some problem 17"<<endl;
     mean_g1xcl = myValues.mean; //
     myValues = I2GFmainLoop(h_residual_g1ycl,1,10,1);
+  if (verbose)
+      cout<<"Some problem 18"<<endl;
     mean_g1ycl = myValues.mean;
+  if (verbose)
+      cout<<"Some problem 19"<<endl;
     
     cout<<"residual mean: "<<mean_g2xcl<<"\t"<<mean_g2ycl<<"\t"<<mean_g3xcl<<"\t"<<mean_g3ycl<<"\t"<<mean_g1xcl<<"\t"<<mean_g1ycl<<endl;
     
--- doubleGausFit_withHistParameter.C
+++ doubleGausFit_withHistParameter.C
@@ -56,6 +56,8 @@ I2GFvalues I2GFmainLoop(TH1F *htemp, int N_iter, float N_sigma_range, bool ShowF
 {
   I2GFvalues myI2GFvalues;
 
+  bool verbose = 0;
+
   //Set initial values...(in case fit fails)
   myI2GFvalues.rchi2 = -100;
   myI2GFvalues.mean = -100;
@@ -114,8 +116,14 @@ I2GFvalues I2GFmainLoop(TH1F *htemp, int N_iter, float N_sigma_range, bool ShowF
   binMaxCnt_value = (Double_t) htemp->GetXaxis()->GetBinCenter(binMaxCnt); //if the bin number is known and the bin value (in x-axis units) is wanted
   binMaxCnt_counts = (Int_t) htemp->GetBinContent(binMaxCnt); //Finds counts within particular bin
 
+  if (verbose)
+  cout<<"binMaxCnt = "<<binMaxCnt<<"\tbinMaxCnt_value = "<<binMaxCnt_value<<"\t binMaxCnt_counts = "<<binMaxCnt_counts<<endl;
+
   //---------TSpectrum Peak Finding Parameters--------
   if(ShowFit) NPeaks = s->Search(htemp, 2,"",0.5); 
+  if (verbose)
+      cout<<"NPeaks = "<<NPeaks<<endl;
+
   /*opens a canvas (one time in a loop), even with:  s->Search(htemp, 2, "nodraw", 0.9);  else  NPeaks = s->Search(htemp, 2, "", 0.5);  //s->Search(htemp, 2, "nodraw", 0.9);
 
   Npeaks = s->GetNPeaks(); //If using this, need pointer in declaration above: Int_t *NPeaks
@@ -123,6 +131,10 @@ I2GFvalues I2GFmainLoop(TH1F *htemp, int N_iter, float N_sigma_range, bool ShowF
 */
   Peak = s->GetPositionX();
   PeakAmp = s->GetPositionY();
+
+
+  if (verbose)
+      cout<<"Peak = "<<Peak[0]<<"\tPeakAmp = "<<PeakAmp[0]<<endl;
 
   for (int i=0; i<NPeaks; i++)
     {
@@ -134,6 +146,12 @@ I2GFvalues I2GFmainLoop(TH1F *htemp, int N_iter, float N_sigma_range, bool ShowF
     }
   peak_pos_bin = htemp->GetXaxis()->FindBin(peak_pos); //if the bin value (in x-axis units) is known and the bin number is wanted
   peak_pos_count = htemp->GetBinContent(peak_pos_bin);  //counts in particular bin
+
+
+  if (verbose)
+      cout<<"peak_pos_bin = "<<peak_pos_bin<<"\tpeak_pos_count = "<<peak_pos_count<<endl;
+
+
 
   //------------------------------------------------------------------------------------------------------------------  
 
@@ -208,8 +226,8 @@ I2GFvalues I2GFmainLoop(TH1F *htemp, int N_iter, float N_sigma_range, bool ShowF
 
 
 
-  //for (int i=0; i< (N_iter - 1); i++)
-  for (int i=0; i< 2; i++)  //8 seems to work well, so let's keep it constant here.
+  for (int i=0; i< (N_iter ); i++)
+  //for (int i=0; i< 2; i++)  //8 seems to work well, so let's keep it constant here.
     {
       //htemp->Fit("gaus", "", "",(f_mean - (N_sigma_range*f_sigma)), (f_mean + (N_sigma_range*f_sigma) ) ); //show fit
       htemp->Fit("gaus", "Q0", "",(f_mean - (N_sigma_range*f_sigma)), (f_mean + (N_sigma_range*f_sigma) ) ); //don't show fit
@@ -227,9 +245,11 @@ cout << " sigma " << f_sigma << endl;
 
   peak =  func->GetParameter(0); //Amplitude
 
+  if (verbose)
+      cout<<"peak = "<<peak<<endl;
   float bgd_h = 0.25; //background height ~ bgd_h*gaus_amp
 
-  func1 = new TF1("func1", "gaus");
+  func1 = new TF1("func1", "gaus",(f_mean - 3*f_sigma), (f_mean + 3*f_sigma));
   //-----------Fit Parameter constraints not needed here (see below):
   //func1->SetParLimits(0, 0, 1000000);
   //func1->SetParameters(f_const, f_mean, f_sigma);    //Here: (Par1 initial value, Par2 initial value, Par3 initial value, etc)
@@ -238,7 +258,7 @@ cout << " sigma " << f_sigma << endl;
   //func1->SetParLimits(1, f_mean-(f_sigma/2),f_mean+(f_sigma/2) );
   //func1->SetParLimits(2, f_sigma-(f_sigma/2),f_sigma+(f_sigma/2) );
   
-  func2 = new TF1("func2", "gaus");
+  func2 = new TF1("func2", "gaus",(f_mean - 3*f_sigma), (f_mean + 3*f_sigma));
   //-----------Fit Parameter constraints not needed here (see below):
   //func2->SetParameters(f_const/10, f_mean, 4*f_sigma); //(const, mean, sigma)     
   //func2->SetParLimits(0,-1,(0.1*peak_pos_amp) );        //(const)     
@@ -253,7 +273,15 @@ cout << " sigma " << f_sigma << endl;
 
   //func2 = new TF1("func2", "pol2");
   
-  func3 = new TF1("func3", "func1 + func2", (f_mean - 3*f_sigma), (f_mean + 3*f_sigma) );
+  if (verbose)
+      cout<<"f_mean = "<<f_mean<<"\tf_sigma = "<<f_sigma<<endl;
+
+  //func3 = new TF1("func3", "func1 + func2", (f_mean - 3*f_sigma), (f_mean + 3*f_sigma) );
+  func3 = new TF1("func3","gaus(0) + gaus(3)",(f_mean - 3*f_sigma), (f_mean + 3*f_sigma));
+  //func3 = new TF1("func3", "func1 ", (f_mean - 3*f_sigma), (f_mean + 3*f_sigma) );
+
+  if (verbose)
+      cout<<"ERROR 1"<<endl;
   //-----------Fit Parameter constraints:
   func3->SetParameters(f_const, f_mean, f_sigma, f_const/10, f_mean, 4*f_sigma); //Set Initial Valules
   //func3->SetParLimits(0, 0, 1000000);
@@ -264,10 +292,16 @@ cout << " sigma " << f_sigma << endl;
   //func3->SetParLimits(5, 0, 1000000);
 
   //htemp->Fit("func3");//, "", "",(f_mean - (N_sigma_range*f_sigma)), (f_mean + (N_sigma_range*f_sigma) ) ); //Show Fit
+  if (verbose)
+      cout<<"ERROR 2"<<endl;
   htemp->Fit("func3", "Q0"); //Don't show fit
+  if (verbose)
+      cout<<"ERROR 3"<<endl;
   func  = htemp->GetFunction("func3");
   Chi2 = func3->GetChisquare();
   NDF = func->GetNDF();
+  if (verbose)
+      cout<<"Chi2 = "<<Chi2<<endl;
   if (NDF != 0) f_RChi2 = Chi2/NDF;
   f_const = func3->GetParameter(0);
   f_mean = func3->GetParameter(1);
@@ -281,6 +315,10 @@ cout << " sigma " << f_sigma << endl;
   f_sigma2 = func3->GetParameter(5);
   f_sigma_err2 = func3->GetParError(5);
   func3->SetParNames("Primary Constant", "Primary Mean", "Primary Sigma", "Background Constant", "Background Mean", "Background Sigma");
+
+
+  if (verbose)
+      cout<<"f_mean = "<<f_mean<<"\tf_mean_err = "<<f_mean_err<<endl;
   //for (int j=0; j< (N_iter - 1); j++)
   for (int j=0; j<4; j++)
     {
@@ -309,6 +347,8 @@ cout << " sigma " << f_sigma << endl;
       f_sigma2 = func3->GetParameter(5);
       f_sigma_err2 = func3->GetParError(5);
     }
+  if (verbose)
+      cout<<"ERROR 4"<<endl;
 
   
 
@@ -333,6 +373,8 @@ cout << " sigma " << f_sigma << endl;
       myI2GFvalues.sigma_err = f_sigma_err2;
       //myI2GFvalues.fit_func = new TF1("fit_func", "func3"); //not needed
     }
+  if (verbose)
+      cout<<"ERROR 5"<<endl;
   
   return myI2GFvalues;
 
